ea78af99f35d1859c8086e8db3ba2d2a5ce137d5
Pedro <pedrozenatte@usp.br>
2026-05-14 15:31:21 -0300

    Atualiza o script de setup para criar o ambiente virtual com o nome .venv

--- automation_frameworks_practice/playwright/setup.sh
+++ automation_frameworks_practice/playwright/setup.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # Criando o ambiente virtual
-python3 -m venv venv
-source venv/bin/activate
+python3 -m venv .venv
+source .venv/bin/activate
 
 # Instalando as dependências
 pip install -r requirements.txt

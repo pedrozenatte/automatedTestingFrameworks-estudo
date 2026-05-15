f99ce4be8c4e089136ad78a62df84aa559421d6e
Pedro <pedrozenatte@usp.br>
2026-05-15 11:18:00 -0300

    Refatorando o setup.sh para utilização do docker

--- automation_frameworks_practice/playwright/setup.sh
+++ automation_frameworks_practice/playwright/setup.sh
@@ -1,27 +1,4 @@
 #!/usr/bin/env bash
 
-# Criando o ambiente virtual
-if [ ! -d ".venv" ]; then
-    echo "Criando ambiente virtual..."
-    python3 -m venv .venv
-else
-    echo "Ambiente virtual já existe. Pulando criação."
-fi
-
-# Ativa a venv
-source .venv/bin/activate
-
-# Atualiza o pip
-pip install --upgrade pip
-
-# Instala as dependências
-pip install -r requirements.txt
-
-# Instala navegadores do Playwright
-if [ ! -f ".playwright_installed" ]; then
-    echo "Instalando navegadores do Playwright..."
-    playwright install
-    touch .playwright_installed
-else
-    echo "Playwright já instalado. Pulando instalação dos navegadores."
-fi
+docker build -t playwright_image .
+docker run --rm --ipc=host playwright_image
